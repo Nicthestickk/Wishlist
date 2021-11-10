fab8f91ec6263357683cc2e24a19568955c728f0
Nicolas <nkristensen1@gmail.com>
2021-11-10 11:05:38 +0100

    added connection

--- src/main/java/ønskeliste/demo/repository/DBManger.java
+++ src/main/java/ønskeliste/demo/repository/DBManger.java
@@ -1,6 +1,12 @@
 package ønskeliste.demo.repository;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBManger {
 
@@ -8,4 +14,26 @@ public class DBManger {
     private static String password;
     private static String url;
     private static Connection connection = null;
+
+
+    public static Connection getConnection(){
+        if (connection != null) {
+            return connection;
+        }
+        try (InputStream input = new FileInputStream("src/main/resources/application.properties")) {
+            Properties properties = new Properties();
+            properties.load(input);
+            url = properties.getProperty("url");
+            username = properties.getProperty("username");
+            password = properties.getProperty("password");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(url,username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
